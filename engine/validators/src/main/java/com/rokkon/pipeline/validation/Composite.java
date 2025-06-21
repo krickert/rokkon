2cccd6096b89b52c1a8afe99847808f3b44057e6
krickert <krickert@gmail.com>
2025-06-21 07:18:30 -0400

    restructured

---
+++ engine/validators/src/main/java/com/rokkon/pipeline/validation/Composite.java
@@ -0,0 +1,16 @@
+package com.rokkon.pipeline.validation;
+
+import jakarta.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Qualifier to indicate the composite pipeline configuration validator.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE})
+public @interface Composite {
+}
